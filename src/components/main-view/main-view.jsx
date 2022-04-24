8f3ad91fd876680070a99e2f40d24d347d49f695
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-04-24 12:31:35 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 
-import { MovieCard } from './movie-card';
+import { LoginView } from '../login-view/login-view';
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/view-movie';
 
 export class MainView extends React.Component {
 
   constructor(){
+ // Initial state is set to null  
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null
     };
   }
 
@@ -24,21 +27,41 @@ export class MainView extends React.Component {
       .catch(error => {
         console.log(error);
       });
+    }
+
+/* When a movie is clicked, this function is invoked and updates the state of the
+'selectedMovie' *property to that movie*/
+
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+/* When a user successfully logs in, this function updates the user's property in state
+to that *particular user*/
+
+  onLoggedIn(user) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      user
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
 
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the
+    user details are *passed as a prop to the LoginView*/
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
+        {/*If the state of 'selectedMovie' is not null, that selected movie will be returned
+        otherwise, all *movies will be returned*/}
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie =>(
