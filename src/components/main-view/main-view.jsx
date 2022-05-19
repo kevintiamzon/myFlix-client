020449651129bb65198fc9f6197f13cf44b258c3
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-05-19 10:55:07 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -19,17 +19,30 @@ export class MainView extends React.Component {
     };
   }
 
-  componentDidMount() {
-    axios.get('https://kevins-movie-api.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
+  getMovies(token) {
+    axios.get('https://kevins-movie-api.herokuapp.com/movies', {
+      headers: { Authorization: 'Bearer ${token}'}
+    })
+    .then(response => {
+      // Assign the result to the state
+      this.setState({
+        movies: response.data
       });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
     }
+  }
 
 /* When a movie is clicked, this function is invoked and updates the state of the
 'selectedMovie' *property to that movie*/
@@ -43,10 +56,15 @@ export class MainView extends React.Component {
 /* When a user successfully logs in, this function updates the user's property in state
 to that *particular user*/
 
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user
+      user: authData.user.Username
     });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   render() {
